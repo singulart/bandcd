47e199a4fe8be2ce09b6b86cb4ab9f5179ebd82a
singulart <serpian@gmail.com>
2016-05-05 12:01:26 +0300

    Changed download script to parse tmp dir files

--- bandown.py
+++ bandown.py
@@ -1,4 +1,6 @@
-import sys
+import sys, os
+import json
+import tempfile
 import wget
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,38 +8,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 print 'Bandcamp album downloader'
+os.chdir(tempfile.gettempdir() + '/freeband.py')
+album_data_files = os.listdir(os.curdir)
+for json_file in album_data_files:
+	with open(json_file, 'r') as infile:
+		json_data = infile.read()
+		album = json.loads(json_data)
 
-url = sys.argv[1]
+		url = album['url']
 
-if not url:
-	print 'usage: bandown.py <album url>'
-	exit(1)
-print 'getting album %s' % url
-driver = webdriver.Firefox()
-driver.get(url)
-buy_now = driver.find_element_by_css_selector('h4 > button.download-link')
-buy_now.click()
+		print 'getting album %s' % url
+		driver = webdriver.Firefox()
+		driver.get(url)
+		buy_now = driver.find_element_by_css_selector('h4 > button.download-link')
+		buy_now.click()
 
-price_field = driver.find_element_by_css_selector("input[id='userPrice']")
-price_field.send_keys('0')
+		price_field = driver.find_element_by_css_selector("input[id='userPrice']")
+		price_field.send_keys('0')
 
-dnow = driver.find_element_by_css_selector("button[onclick='TralbumDownload.checkout(); return false']")
-dnow.click()
+		dnow = driver.find_element_by_css_selector("button[onclick='TralbumDownload.checkout(); return false']")
+		dnow.click()
 
-try:
-	format_drop_down = driver.find_element_by_id('downloadFormatMenu0')
-	format_drop_down.click()
-except:
-	print 'Album %s cannot be downloaded: email-based links are not supported' % url
-	driver.close()
-	exit(1)
+		try:
+			format_drop_down = driver.find_element_by_id('downloadFormatMenu0')
+			format_drop_down.click()
+		except:
+			print 'Album %s cannot be downloaded: email-based links are not supported' % url
+			driver.close()
+			exit(1)
 
-flac = driver.find_element_by_css_selector("li[data-value='flac']")
-flac.click()
+		flac = driver.find_element_by_css_selector("li[data-value='flac']")
+		flac.click()
 
-go = WebDriverWait(driver, 60).until(
-		EC.presence_of_element_located((By.CSS_SELECTOR, "a.downloadGo"))
-	)
+		go = WebDriverWait(driver, 60).until(
+				EC.presence_of_element_located((By.CSS_SELECTOR, "a.downloadGo"))
+			)
 
-wget.download(go.get_attribute('href'))
-driver.quit()
+		wget.download(go.get_attribute('href'))
+		driver.quit()
